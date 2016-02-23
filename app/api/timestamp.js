dea1ba75c5c3e2b626a1cadf4ab489a808426b66
brooksofmaine <brooksofmaine@gmail.com>
2016-02-23 23:08:57 +0000

    check for invalid date and print null

--- app/api/timestamp.js
+++ app/api/timestamp.js
@@ -7,22 +7,28 @@ module.exports = function (app){
         var date = req.params.query;
         var unix = null;
         var natural = null;
+         
         //check for initial unix time
-        if( +date >= 0){
+        if( +date >= 0 ){
             unix = +date;
             natural = unixToNat(unix);
-        }
-        //check for initial natural time 
-        if (isNaN(+date) && moment(date, "MMMM D, YYYY").isValid){
+             var dateObj = { "unix": unix, "natural": natural };
+        }//close if for unix
+        //check for initial natural time
+      else if ( moment(date, "MMMM D, YYYY").isValid) {
             unix = natToUnix(date);
             natural = unixToNat(unix);
-        }
-        var dateObj = { "unix": unix, "natural": natural };
-
+            //make sure we don't have an invalid date
+            if (natural != "Invalid date"){  
+                var dateObj = { "unix": unix, "natural": natural };
+            }//close if
+            else {
+            var dateObj = { "unix": null, "natural": null }; 
+             }//close else
+      }//close else if for natural
       
-             res.send(JSON.stringify(dateObj));
-  
-    });
+      res.send(JSON.stringify(dateObj));
+      
     function natToUnix(date){
         //convert from natural date to unix timestamp
         return moment(date, "MMMM D, YYYY").format("X");
@@ -30,6 +36,7 @@ module.exports = function (app){
     function unixToNat(unix){
         //convert from unix to naturaldate
         return moment.unix(unix).format("MMMM D, YYYY");
-    }
-    
-};
+    }//close unixToNat
+  }//close anon for app.get
+ )//close app.get  
+};//close module.exports
