b3c9df5eb3709429f8647d60c3c993c3474903da
brooksofmaine <brooksofmaine@gmail.com>
2016-02-23 18:15:32 +0000

    delete unused code and add credits

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 
-  This server returns the date in unix and natural form.
+  This server returns the date in unix and natural form. The challenge for me lay in discovering
+  how node and heroku work. Special thanks to Raphael Rodriguez for providing a model.  Also thanks to 
+  Steve Katz at https://www.youtube.com/watch?v=uLF_hmtxAsY for walking me through the process of heroku
+  
--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 
-// my input
+// special thanks to Raphael Rodriguez for providing a model
 'use strict';
 var express = require('express');
 var app = express();
@@ -21,33 +21,3 @@ api(app);
 app.listen(port, function(){
   console.log('listening on ' + process.env.PORT);
 });
-
-//Raphael's input
-/*
-'use strict';
-
-// call the packages we need
-var express    = require('express');        // call express
-var app        = express();                 // define our app using express
-var bodyParser = require('body-parser');
-var routes = require('./app/routes/index.js');
-var api = require('./app/api/timestamp.js');
-
-// configure app to use bodyParser()
-// this will let us get the data from a POST
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-app.use('/public', express.static(process.cwd() + '/public'));
-    
-var port = process.env.PORT || 8080;        // set our port
-    
-// The format follows as, alias to use for real path, also allows permission to such path.
-//app.use('/api', express.static(process.cwd() + '/app/api'));
-    
-routes(app);
-api(app);
-
-app.listen(port, function() {
-    console.log('Node.js listening on port ' + port);
-});
-*/
